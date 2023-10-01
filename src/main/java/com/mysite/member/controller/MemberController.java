9d2775e2631f160adb97afa588d26302780dcf8d
jssa <jinwoo2328@gmail.com>
2023-10-01 17:28:45 +0900

    first

---
+++ src/main/java/com/mysite/member/controller/MemberController.java
@@ -0,0 +1,19 @@
+package com.mysite.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+    // 회원가입 페이지 출력 요청
+    @GetMapping("member/save")
+    private String saveForm(){
+        return "save";
+    }
+    @PostMapping("member/save")
+    public String save(){
+        System.out.println("MemberController.save");
+        return null;
+    }
+}

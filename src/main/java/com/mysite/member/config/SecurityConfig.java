fa33e625938bfb0bbb202e6e1059dff4c3a4efcf
jssa <jinwoo2328@gmail.com>
2024-02-19 19:06:46 +0900

    SecurityConfig 수정 중

--- src/main/java/com/mysite/member/config/SecurityConfig.java
+++ src/main/java/com/mysite/member/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.mysite.member.config;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,29 +9,29 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
+
         return new BCryptPasswordEncoder();
     }
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/oauth2/**", "/member/login", "/login", "/join", "/joinProc").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
-                        .anyRequest().authenticated());
+                        // 권한은 순서대로 줘야한다. 특정 권한을 다 주고 나서 나머지를 맨 마지막에 anyRequest로 지정
+                        .requestMatchers("/", "/member/login", "/member/save").permitAll()
+                        .requestMatchers("/member/list").hasRole("ADMIN")
+                        .requestMatchers("/member/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
         http
-                .httpBasic((basic) -> basic.disable());
+                .formLogin((auth) -> auth.loginPage("/member/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
+                );
         http
-                .csrf((csrf) -> csrf.disable());
-        http
-                .formLogin((login) -> login
-                                .loginPage("/login")
-                                .loginProcessingUrl("/member/login")
-                                .permitAll());
+                .csrf((auth) -> auth.disable());
         return http.build();
     }
 }
